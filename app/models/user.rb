1b0c7e3825aded6e7e60f5eeec54fa34013874b9
Rayson <raysonlcp1223@gmail.com>
2014-11-03 19:30:21 +0800

    Update user validations + views

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	has_secure_password
+    
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :email,format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+    validates :password, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Workspace::Application.routes.draw do
 
+  resources :users
+  get 'sign-up' => 'users#new'
+
   get 'about' => 'welcome#about'
 
   get 'contact' => 'welcome#contact'
